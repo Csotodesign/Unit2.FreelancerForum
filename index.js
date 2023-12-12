5d9c18d452d9ff540c691728ab74180a7a41a5c7
crystal soto <crystalsotogd@yahoo.com>
2023-12-12 17:11:36 -0600

    Cleaned up syntax copy and formatted code

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const skills = ['Web Development', 'Graphic Design', 'Writing', 'Marketing', 'Vi
 function generateRandomFreelancer() {
     const name = freelancerNames[Math.floor(Math.random() * freelancerNames.length)];
     const skill = skills[Math.floor(Math.random() * skills.length)];
-    const hourlyRate = Math.floor(Math.random() * 50) + 20; // Random hourly rate between 20 and 70
+    const hourlyRate = Math.floor(Math.random() * 50) + 20; // Random starting price between 20 and 70
     return { name, skill, hourlyRate };
   }
 
-// Function to display a freelancer
+// Function to display and render the random freelancers
 function displayFreelancer(freelancer) {
-    console.log(`Name: ${freelancer.name}, Skill: ${freelancer.skill}, Hourly Rate: $${freelancer.hourlyRate}`);
+    const freelancersList = document.getElementById('freelancersList');
+    const listItem = document.createElement('li');
+    listItem.textContent = `Name: ${freelancer.name}, Occupation: ${freelancer.skill}, Starting Price: $${freelancer.hourlyRate}`;
+    freelancersList.appendChild(listItem);
   }
 
 // Function to reflect the average starting price
@@ -21,23 +24,28 @@ function updateAveragePrice(freelancers) {
   const totalHourlyRate = freelancers.reduce((sum, freelancer) => sum + freelancer.hourlyRate, 0);
   const averagePrice = totalHourlyRate / freelancers.length;
   const averagePriceElement = document.getElementById('averagePrice');
-  averagePriceElement.textContent = `The average Starting Price: $${averagePrice.toFixed(2)}`;
+  averagePriceElement.textContent = `The average starting price is $${averagePrice.toFixed(2)}`;
 }
 
 // Function to generate random freelancers at intervals
 function generateRandomFreelancersAtIntervals(interval, numberOfFreelancers) {
     let counter = 0;
+    const freelancers = [];
+
     const intervalId = setInterval(() => {
       const freelancer = generateRandomFreelancer();
       displayFreelancer(freelancer);
+      freelancers.push(freelancer);
+      updateAveragePrice(freelancers);
+
       counter++;
-  
+
       if (counter === numberOfFreelancers) {
-        clearInterval(intervalId); // Stop the interval after generating the desired number of freelancers
+        clearInterval(intervalId);
       }
     }, interval);
   }
   
 
-//Generate 5 random freelancers every 2 seconds
-generateRandomFreelancersAtIntervals(2000, 5);
+//Generate 5 random freelancers every 1 seconds
+generateRandomFreelancersAtIntervals(1000, 3);
